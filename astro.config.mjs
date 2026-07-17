a3a0438b5069a1d65879b1e2e1a39696a52d0217
Anna Murphy <curunilauro@gmail.com>
2026-07-16 20:37:39 -0400

    CI: v3, update astro path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://pvd-meshworks.github.io/",
+  base: "/blerg",
   fonts: [
     {
       provider: fontProviders.google(),
